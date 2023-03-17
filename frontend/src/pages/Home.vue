76cb61b46502ea4532149f94634b9e098b6b2669
javaYuhm <kora1492@naver.com>
2023-03-17 17:49:18 +0900

    SpringBoot + Vue.js 프로젝트 1일차 프론트엔드 프로젝트생성

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -18,8 +18,9 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-            <Card></Card>
+          <div class="col" v-for="(item, idx) in state.items" :key="idx">
+            <Card :item="item" />
+
           </div>
         </div>
       </div>
@@ -30,9 +31,23 @@
 
 <script>
 import Card from "@/components/Card";
+import axios from "axios";
+import {reactive} from "vue";
 export default {
   name: "Home",
-  components: {Card}
+  components: {Card},
+  setup(){
+    const state = reactive({
+      items:[]
+    })
+
+    axios.get("/api/items").then((res) => {
+      console.log(res);
+      state.items = res.data;
+    })
+
+    return {state}
+  }
 }
 
 </script>
