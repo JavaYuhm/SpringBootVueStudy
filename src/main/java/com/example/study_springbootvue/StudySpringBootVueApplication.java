1cb9ddb82b0dc246bb8a36d7f1df98b0aa6b6033
javaYuhm <kora1492@naver.com>
2023-03-17 17:17:28 +0900

    SpringBoot + Vue.js 프로젝트 1일차 프론트엔드 프로젝트생성

---
+++ src/main/java/com/example/study_springbootvue/StudySpringBootVueApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootVueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/study_springbootvue/StudySpringBootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_springbootvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
